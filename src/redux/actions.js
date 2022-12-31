f3a9c5665a7b83be17b9e158c748e6a919309666
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-31 17:46:54 +0200

    rewrote actions on redux-toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,33 +1,13 @@
+import { createAction } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
-export const addTask = text => {
-  return {
-    type: 'tasks/addTask',
-    payload: {
-      id: nanoid(),
-      completed: false,
-      text,
-    },
-  };
-};
-
-export const deleteTask = taskId => {
-  return {
-    type: 'tasks/deleteTask',
-    payload: taskId,
-  };
-};
-
-export const toggleCompleted = taskId => {
-  return {
-    type: 'tasks/toggleCompleted',
-    payload: taskId,
-  };
-};
-
-export const setStatusFilter = value => {
-  return {
-    type: 'filters/setStatusFilter',
-    payload: value,
-  };
-};
+export const addTask = createAction('tasks/addTask', text => ({
+  payload: {
+    text,
+    id: nanoid(),
+    completed: false,
+  },
+}));
+export const deleteTask = createAction('tasks/deleteTask');
+export const toggleCompleted = createAction('tasks/toggleCompleted');
+export const setStatusFilter = createAction('tasks/setStatusFilter');
