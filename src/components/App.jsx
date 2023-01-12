3258b9743e55486949f19d3223b1ae60669f92ba
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-01-12 22:33:11 +0200

    added adding operation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,50 @@
-// import { Layout } from 'components/Layout/Layout';
-// import { AppBar } from 'components/AppBar/AppBar';
-// import { TaskForm } from 'components/TaskForm/TaskForm';
-// import { TaskList } from 'components/TaskList/TaskList';
-
-// export const App = () => {
-//   return (
-//     <Layout>
-//       <AppBar />
-//       <TaskForm />
-//       <TaskList />
-//     </Layout>
-//   );
-// };
-
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from 'redux/operations';
-import { getTasks } from 'redux/selectors';
+import { getError, getIsLoading } from 'redux/selectors';
+
+import { Layout } from 'components/Layout/Layout';
+import { AppBar } from 'components/AppBar/AppBar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
 
 export const App = () => {
   const dispatch = useDispatch();
-  // Отримуємо частини стану
-  const { items, isLoading, error } = useSelector(getTasks);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
-  // Рендерим розмітку в залежності від значень у стані
+
   return (
-    <div>
-      {isLoading && <p>Loading tasks...</p>}
-      {error && <p>{error}</p>}
-      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
-    </div>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <TaskList />
+    </Layout>
   );
 };
+
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchTasks } from 'redux/operations';
+// import { getTasks } from 'redux/selectors';
+
+// export const App = () => {
+//   const dispatch = useDispatch();
+//   // Отримуємо частини стану
+//   const { items, isLoading, error } = useSelector(getTasks);
+//   useEffect(() => {
+//     dispatch(fetchTasks());
+//   }, [dispatch]);
+//   // Рендерим розмітку в залежності від значень у стані
+//   return (
+//     <div>
+//       {isLoading && <p>Loading tasks...</p>}
+//       {error && <p>{error}</p>}
+//       <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
+//     </div>
+//   );
+// };
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,8 +1,8 @@
-import {
-  fetchingInProgress,
-  fetchingSuccess,
-  fetchingError,
-} from './taskSlice';
+// import {
+//   fetchingInProgress,
+//   fetchingSuccess,
+//   fetchingError,
+// } from './taskSlice';
 
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
@@ -19,7 +19,38 @@ axios.defaults.baseURL = 'https://63c01c12a177ed68abbfc883.mockapi.io';
 //   }
 // };
 
-export const fetchTasks = createAsyncThunk('tasks/fetchAll', async () => {
-  const response = await axios.get('/tasks/current');
-  return response.data;
-});
+export const fetchTasks = createAsyncThunk(
+  'tasks/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/tasks/current');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addTask = createAsyncThunk(
+  'tasks/addTask',
+  async (text, thunkAPI) => {
+    try {
+      const response = await axios.post('/tasks/current', { text });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteTask = createAsyncThunk(
+  'tasks/deleteTask',
+  async (taskId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/tasks/current/${taskId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
