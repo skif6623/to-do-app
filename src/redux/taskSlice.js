edcc2ab5a7cdf74396e4a84b96ed62406cfe6f86
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-01-12 23:04:47 +0200

    shortened the code of reducers

--- src/redux/taskSlice.js
+++ src/redux/taskSlice.js
@@ -1,14 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchTasks, addTask, deleteTask, toggleCompleted } from './operations';
-// import { nanoid } from 'nanoid';
 
-// [
-//   { id: 0, text: 'Learn HTML and CSS', completed: true },
-//   { id: 1, text: 'Get good at JavaScript', completed: true },
-//   { id: 2, text: 'Master React', completed: false },
-//   { id: 3, text: 'Discover Redux', completed: false },
-//   { id: 4, text: 'Build amazing apps', completed: false },
-// ]
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const taskinitialState = {
   items: [],
@@ -20,35 +20,26 @@ const tasksSlice = createSlice({
   name: 'tasks',
   initialState: taskinitialState,
   extraReducers: {
-    // ЧИТАННЯ З БЕКУ
-    [fetchTasks.pending](state) {
-      state.isLoading = true;
-    },
+    // PENDING
+    [fetchTasks.pending]: handlePending,
+    [addTask.pending]: handlePending,
+    [deleteTask.pending]: handlePending,
+    [toggleCompleted.pending]: handlePending,
+    // REJECTED
+    [fetchTasks.rejected]: handleRejected,
+    [addTask.rejected]: handleRejected,
+    [deleteTask.rejected]: handleRejected,
+    [toggleCompleted.rejected]: handleRejected,
+    // FULFILLED
     [fetchTasks.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = action.payload;
     },
-    [fetchTasks.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    // ДОДАВАННЯ В БЕК
-    [addTask.pending](state) {
-      state.isLoading = true;
-    },
     [addTask.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items.unshift(action.payload);
-    },
-    [addTask.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    // ВИДАЛЕННЯ З БЕКУ
-    [deleteTask.pending](state) {
-      state.isLoading = true;
     },
     [deleteTask.fulfilled](state, action) {
       state.isLoading = false;
@@ -57,14 +48,6 @@ const tasksSlice = createSlice({
         task => task.id === action.payload.id
       );
       state.items.splice(index, 1);
-    },
-    [deleteTask.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    // ЗМІНА СТАТУСУ
-    [toggleCompleted.pending](state) {
-      state.isLoading = true;
     },
     [toggleCompleted.fulfilled](state, action) {
       state.isLoading = false;
