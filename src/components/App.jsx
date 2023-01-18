c8a1b48e8e2573fa8e2153927b18009929767c26
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-01-18 21:49:57 +0200

    code review App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,25 +26,3 @@ export const App = () => {
     </Layout>
   );
 };
-
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchTasks } from 'redux/operations';
-// import { getTasks } from 'redux/selectors';
-
-// export const App = () => {
-//   const dispatch = useDispatch();
-//   // Отримуємо частини стану
-//   const { items, isLoading, error } = useSelector(getTasks);
-//   useEffect(() => {
-//     dispatch(fetchTasks());
-//   }, [dispatch]);
-//   // Рендерим розмітку в залежності від значень у стані
-//   return (
-//     <div>
-//       {isLoading && <p>Loading tasks...</p>}
-//       {error && <p>{error}</p>}
-//       <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
-//     </div>
-//   );
-// };
