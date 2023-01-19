b4ba57c3a20abfae899696226b7290d30bda0bca
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-01-19 21:22:11 +0200

    code reviev

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,23 +1,7 @@
-// import {
-//   fetchingInProgress,
-//   fetchingSuccess,
-//   fetchingError,
-// } from './taskSlice';
-
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://63c44b33f0028bf85fa64c21.mockapi.io';
-
-// export const fetchTasks = () => async dispatch => {
-//   try {
-//     dispatch(fetchingInProgress());
-//     const response = await axios.get('/tasks/current');
-//     dispatch(fetchingSuccess(response.data));
-//   } catch (error) {
-//     dispatch(fetchingError(error.message));
-//   }
-// };
 
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { tasksReducer } from './taskSlice';
 import { filtersReducer } from './filtersSlice';
-// import { taskReducer, filtersReducer } from './reducer';
 
 export const store = configureStore({
   reducer: {
